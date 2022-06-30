69fd2bc8fac4061f30f9023e6d9f1b24598ffbea
ECEEvanCampbell <evan.campbell1@unb.ca>
2022-06-29 23:49:12 -0300

    made sgt window base class

---
+++ screenGuidedTrainingWindow.py
@@ -0,0 +1,19 @@
+from utils import PageWindow
+from PyQt5 import QtCore, QtWidgets
+
+class ScreenGuidedTrainingWindow(PageWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Screen Guided Collection")
+        self.UiComponents()
+
+    def goToMain(self):
+        self.goto("main")
+
+    def UiComponents(self):
+        self.backButton = QtWidgets.QPushButton("BackButton", self)
+        self.backButton.setGeometry(QtCore.QRect(5, 5, 100, 20))
+        self.backButton.clicked.connect(self.goToMain)
