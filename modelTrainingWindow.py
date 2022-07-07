246884da94bd90e54ee844ccab3316ab595e4bf5
ECEEvanCampbell <evan.campbell1@unb.ca>
2022-07-06 21:04:39 -0300

    Added base model training page

--- modelTrainingWindow.py
+++ modelTrainingWindow.py
@@ -1,9 +1,20 @@
 from utils import PageWindow
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtWidgets import QComboBox, QMessageBox
 
 class ModelTrainingWindow(PageWindow):
-    def __init__(self):
+    def __init__(self, basewindow):
         super().__init__()
+        self.setObjectName("settings_window")
+        self.resize(514, 466)
+        self.massive_font = QtGui.QFont()
+        self.massive_font.setPointSize(22)
+        self.title_font = QtGui.QFont()
+        self.title_font.setPointSize(16)
+        self.subtitle_font = QtGui.QFont()
+        self.subtitle_font.setPointSize(14)
+        self.text_font  = QtGui.QFont()
+        self.text_font.setPointSize(11)
         self.initUI()
 
     def initUI(self):
@@ -14,6 +25,9 @@ class ModelTrainingWindow(PageWindow):
         self.goto("main")
 
     def UiComponents(self):
-        self.backButton = QtWidgets.QPushButton("BackButton", self)
-        self.backButton.setGeometry(QtCore.QRect(5, 5, 100, 20))
-        self.backButton.clicked.connect(self.goToMain)
+        ## CENTRAL WIDGET - this is a parent widget for formatting
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.setCentralWidget(self.centralwidget)
+
+        
