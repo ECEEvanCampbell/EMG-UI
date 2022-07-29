d67c5cfbd47e0c87d9f2e18eb4b5e5d3b6e4e4de
ECEEvanCampbell <evan.campbell1@unb.ca>
2022-07-29 09:18:31 -0700

    Added rejection & thresholding

--- modelManualTrainingWindow.py
+++ modelManualTrainingWindow.py
@@ -1,6 +1,6 @@
 from utils import PageWindow
 from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtWidgets import QComboBox, QMessageBox, QLabel, QLineEdit
+from PyQt5.QtWidgets import QComboBox, QMessageBox, QLabel, QLineEdit, QCheckBox
 from pr_utils import EMGClassifier
 import os
 
@@ -66,8 +66,25 @@ class ManualModelTrainingWindow(PageWindow):
         self.manual_model_classifier_box.setGeometry(QtCore.QRect(5, 355, 190, 30))
         self.manual_model_classifier_box.addItems(["LDA", "SVM"])
 
+        self.manual_active_threshold_label = QLabel(self.centralwidget)
+        self.manual_active_threshold_label.setGeometry(QtCore.QRect(5,390, 165,30))
+        self.manual_active_threshold_label.setText("Active Threhsold")
+
+        self.manual_active_threshold_toggle = QCheckBox(self.centralwidget)
+        self.manual_active_threshold_toggle.setGeometry(QtCore.QRect(160, 390, 30,30))
+
+
+        self.manual_rejection_label = QLabel(self.centralwidget)
+        self.manual_rejection_label.setGeometry(QtCore.QRect(5, 415, 190, 30))
+        self.manual_rejection_label.setText("Rejection Threshold")
+
+        self.manual_rejection_threshold = QLineEdit(self.centralwidget)
+        self.manual_rejection_threshold.setGeometry(QtCore.QRect(5,450, 190, 30))
+        self.manual_rejection_threshold.setText("No")
+
+
         self.manual_button = QtWidgets.QPushButton(self.centralwidget)
-        self.manual_button.setGeometry(QtCore.QRect(5, 390, 190, 30))
+        self.manual_button.setGeometry(QtCore.QRect(5, 485, 190, 30))
         self.manual_button.setText("Make Model")
         self.manual_button.clicked.connect(self.make_manual_model)
 
@@ -75,13 +92,22 @@ class ManualModelTrainingWindow(PageWindow):
         features = self.manual_model_feature_box.currentText()
         classifier = self.manual_model_classifier_box.currentText()
         data_filename = self.manual_data_filename_input.text()
-        window_size = self.manual_window_size_label.text()
-        window_increment = self.manual_window_inc_input.text()
+        window_size = float(self.manual_window_size_input.text())*1000
+        window_increment = float(self.manual_window_inc_input.text())*1000
+
+        active_threshold = self.manual_active_threshold_toggle.isChecked()
+        rejection_threshold = self.manual_rejection_threshold.text()
+        if rejection_threshold == "No":
+            rejection_threshold = False
+        elif rejection_threshold == "ROC": # choose best threshold
+            rejection_threshold = True
+        else:
+            rejection_threshold = float(rejection_threshold) # use specific threshold
 
         frequency = 1259 # TODO: dont't just hardcode this for delsys
 
         window_parameters = [window_size, window_increment, frequency]
-        self.basewindow.model = EMGClassifier("manual", [features, classifier], data_filename, window_parameters)
+        self.basewindow.model = EMGClassifier("manual", [features, classifier], data_filename, window_parameters, active_threshold, rejection_threshold)
         # POPUP saying the classifier is trained
         msg = QMessageBox()
         msg.setWindowTitle("Classifier Trained")
